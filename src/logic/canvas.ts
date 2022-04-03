78f455ff00b8e11dc2117eec0477dc6e20f1cc2b
Elliot67 <elliot.lintz@gmail.com>
2022-04-03 20:37:13 +0200

    :sparkles: scale down big favicon

--- src/logic/canvas.ts
+++ src/logic/canvas.ts
@@ -22,15 +22,27 @@ export function createCanvasWithImage($img: HTMLImageElement): {
   $canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
 } | null {
+  let imgWidth = $img.width;
+  let imgHeight = $img.height;
+
+  // Resize image with a max size boundary
+  const MAX_IMG_DIMENSION = 512;
+  if (imgWidth > MAX_IMG_DIMENSION || imgHeight > MAX_IMG_DIMENSION) {
+    const scaleRatio = Math.max(imgWidth / MAX_IMG_DIMENSION, imgHeight / MAX_IMG_DIMENSION);
+    imgWidth = imgWidth / scaleRatio;
+    imgHeight = imgHeight / scaleRatio;
+  }
+
   const $canvas = document.createElement('canvas');
-  $canvas.width = $img.width;
-  $canvas.height = $img.height;
+  $canvas.width = imgWidth;
+  $canvas.height = imgHeight;
   const ctx = $canvas.getContext('2d');
   if (isNull(ctx)) {
     console.warn('Error, could not get canvas context');
     return null;
   }
-  ctx.drawImage($img, 0, 0);
+
+  ctx.drawImage($img, 0, 0, imgWidth, imgHeight);
   return { $canvas, ctx };
 }
 
